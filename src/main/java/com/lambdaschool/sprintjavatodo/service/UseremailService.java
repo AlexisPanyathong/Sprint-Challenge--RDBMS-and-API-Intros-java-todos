28d6dce76fcafdb04b1f221aba6e8c8fa9ea1894
Alexis Panyathong <alexispanyathong@gmail.com>
2019-10-11 15:59:34 -0500

    Progress - I added the Useremail Controller, Repository, Service, Model, and I added the H2ServerConfig. I am getting some oauth2 import errors and don't know what to do to fix them...

---
+++ src/main/java/com/lambdaschool/sprintjavatodo/service/UseremailService.java
@@ -0,0 +1,26 @@
+package com.lambdaschool.sprintjavatodo.service;
+
+import com.lambdaschool.oauth2.models.Useremail;
+import com.lambdaschool.sprintjavatodo.model.Useremail;
+
+import java.util.List;
+
+public interface UseremailService
+{
+    List<Useremail> findAll();
+
+    Useremail findUseremailById(long id);
+
+    List<Useremail> findByUserName(String username,
+                                   boolean isAdmin);
+
+    void delete(long id,
+                boolean isAdmin);
+
+    Useremail update(long useremailid,
+                     String emailaddress,
+                     boolean isAdmin);
+
+    // note emails are added through the user process
+}
+
