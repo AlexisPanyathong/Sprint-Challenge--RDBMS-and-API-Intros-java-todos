97ea9eb9fc2c78e1ca98239c0d07569787ac498d
Alexis Panyathong <alexispanyathong@gmail.com>
2019-10-11 11:38:33 -0500

    Progress - Finished up all of the models.

--- src/main/java/com/lambdaschool/sprintjavatodo/model/ToDo.java
+++ src/main/java/com/lambdaschool/sprintjavatodo/model/ToDo.java
@@ -1,4 +1,85 @@
 package com.lambdaschool.sprintjavatodo.model;
 
-public class ToDo {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "todos")
+public class ToDo extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long todoid;
+
+    @Column(nullable = false)
+    private String description;
+
+    // Fetching userid
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userid",
+                nullable = false)
+    @JsonIgnoreProperties({"todos", "hibernateInitializer"})
+    private User user;
+
+    // Complete functionality
+    private boolean completed;
+
+    private Date datestarted;
+
+    // Constructor of complete
+    public ToDo() {
+        this.completed = false;
+    }
+
+    // Constructor
+    public ToDo(String description, User user, Date datestarted) {
+        this.description = description;
+        this.user = user;
+        this.completed = false;
+        this.datestarted = datestarted;
+    }
+
+    // Getters and Setters
+
+    public long getTodoid() {
+        return todoid;
+    }
+
+    public void setTodoid(long todoid) {
+        this.todoid = todoid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Date getDatestarted() {
+        return datestarted;
+    }
+
+    public void setDatestarted(Date datestarted) {
+        this.datestarted = datestarted;
+    }
 }
--- src/main/java/com/lambdaschool/sprintjavatodo/model/User.java
+++ src/main/java/com/lambdaschool/sprintjavatodo/model/User.java
@@ -1,4 +1,115 @@
 package com.lambdaschool.sprintjavatodo.model;
 
-public class User {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "users")
+public class User extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userid;
+
+    @Column(nullable = false,
+            unique = true)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    // Mapping over user to UserRole
+    @OneToMany(mappedBy = "user",
+                cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("user")
+    private List<UserRoles> userRoles = new ArrayList<>();
+
+    // Mapping over user to todo
+    @OneToMany(mappedBy = "user",
+                cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("user")
+    private List<ToDo> todos = new ArrayList<>();
+
+    // Empty Constructor
+    public User() {
+    }
+
+    // Constructor
+    public User(String username, String password, List<UserRoles> userRoles) {
+        this.username = username;
+        this.password = password;
+
+        for (UserRoles ur : userRoles) {
+
+            ur.setUser(this);
+        }
+        this.userRoles = userRoles;
+    }
+
+    // Getters and Setters
+
+    public long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(long userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public void setPasswordNoEncrypt(String password)
+    {
+        this.password = password;
+    }
+
+    public List<UserRoles> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(List<UserRoles> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    public List<ToDo> getTodos() {
+        return todos;
+    }
+
+    public void setTodos(List<ToDo> todos) {
+        this.todos = todos;
+    }
+
+    //
+    public List<SimpleGrantedAuthority> getAuthority() {
+
+        List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+
+        for (UserRoles r : this.userRoles) {
+
+            String myRole = "ROLE_" + r.getRole().getName().toUpperCase();
+            rtnList.add(new SimpleGrantedAuthority((myRole)));
+        }
+        return rtnList;
+    }
 }
